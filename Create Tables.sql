2110fe50a1ff43fa5641ca7cbe5bcd6c7cefdc8d
evairdaniel <39104875+evairdaniel@users.noreply.github.com>
2022-06-27 18:28:13 -0300

    Update Create Tables.sql

--- Create Tables.sql
+++ Create Tables.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS estacionamento;
 
-CREATE TABLE tbl_movimentacao (
+CREATE TABLE estacionamento.tbl_movimentacao (
   id int NOT NULL AUTO_INCREMENT,
   placa char(7) NOT NULL,
   modelo char(40) NOT NULL,
@@ -10,14 +10,14 @@ CREATE TABLE tbl_movimentacao (
   valor_pago decimal(8,3) DEFAULT NULL,
   PRIMARY KEY (id)
 );
-CREATE TABLE tbl_usuario (
+CREATE TABLE estacionamento.tbl_usuario (
   id int NOT NULL AUTO_INCREMENT,
   nome char(60) DEFAULT NULL,
   usuario char(20) NOT NULL,
   senha char(6) NOT NULL,
   PRIMARY KEY (id)
 ) ;
-CREATE TABLE tbl_valor (
+CREATE TABLE estacionamento.tbl_valor (
   id int NOT NULL AUTO_INCREMENT,
   valor_primeira_hora decimal(8,3) NOT NULL,
   valor_demais_horas decimal(8,3) NOT NULL,
